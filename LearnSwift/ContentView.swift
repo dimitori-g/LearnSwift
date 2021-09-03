b19a4723063e1af88713ed15c9911fb75596a4a2
Dmitry <dmitry@arche1.co.jp>
2021-09-03 13:05:16 +0900

    Implement reset button

--- LearnSwift/ContentView.swift
+++ LearnSwift/ContentView.swift
@@ -10,24 +10,31 @@ import SwiftUI
 struct ContentView: View {
     @State var i: Int = 0
     var body: some View {
-        HStack{
-            ZStack{
-                Circle()
-                    .fill(Color.green)
-                Button(action: {
-                    i += 1
-                }, label: {
-                    Text("Increment")
-                })
+        VStack{
+            HStack{
+                ZStack{
+                    Circle()
+                        .fill(Color.green)
+                    Button(action: {
+                        i += 1
+                    }, label: {
+                        Text("Increment")
+                    })
+                }
+                .frame(width: 100, height: 100)
+                VStack(alignment:.leading, spacing:4){
+                    Text("Count \(i)")
+                        .font(.largeTitle)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.gray)
+                    Text("Dmitry, 2021")
+                }
             }
-            .frame(width: 100, height: 100)
-            VStack(alignment:.leading, spacing:4){
-                Text("Count \(i)")
-                    .font(.largeTitle)
-                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(.gray)
-                Text("Dmitry, 2021")
-            }
+            Button(action: {
+                i = 0
+            }, label: {
+                Text("Reset")
+            })
         }
     }
 }
