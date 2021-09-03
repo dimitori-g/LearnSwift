c6bfcba6a156bc64052b39cd140eb1e266b97266
Dmitry <dmitry@arche1.co.jp>
2021-09-03 09:41:25 +0900

    Initial Commit

---
+++ LearnSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnSwift
+//
+//  Created by arche1 on 2021/09/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnSwift/LearnSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftApp.swift
+//  LearnSwift
+//
+//  Created by arche1 on 2021/09/03.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
